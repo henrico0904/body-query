b9ed852a9f4e0ec2520c1ff21cb6623aa773cf72
Henrico <henrico.ferreira@aluno.senai.br>
2025-08-29 16:12:21 -0300

    feat: adiciona paramêtros query

--- server.js
+++ server.js
@@ -21,6 +21,32 @@ app.get("/", (req, res) => {
 
 // Aqui vão todas suas Rotas
 
+// Query Parameters no Node.js - API de Hogwarts
+app.get('/bruxos', (req, res) => {
+    const { casa, ano, especialidade, nome } = req.query;
+    let resultado = bruxos;
+  
+    if (casa) {
+      resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
+    }
+  
+    if (ano) {
+      resultado = resultado.filter(b => b.ano == ano);
+    }
+  
+    if (especialidade) {
+      resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
+    }
+  
+    if (nome) {
+      resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+  
+    res.status(200).json({
+      total: resultado.length,
+      data: resultado
+    });
+});
 
 // Iniciar servidor escutando na porta definida
 app.listen(serverPort, () => {
